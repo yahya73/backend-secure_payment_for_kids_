0d3d0873af8e7094d64fbefa3d7e80df99d82001
mohamed chebbi <chebbim4@gmail.com>
2024-02-27 11:19:12 +0100

    socket needs a little tinkering

--- server.js
+++ server.js
@@ -2,41 +2,49 @@ import express from "express";
 import mongoose from "mongoose";
 import morgan from "morgan";
 import cors from "cors";
+import { Server } from "socket.io"; // Import socket.io
+import http from "http"; // Import http module
 import { notFoundError } from "./middlewares/error-handler.js";
 import { errorHandler } from "./middlewares/error-handler.js";
 import Routes from "./routes/Routes.js";
-import PartenaireRoutes from "./routes/partenaireRoutes.js"
-import ParentRoutes from './routes/parentRoutes.js'
-import dotenv from 'dotenv';
-import user from './routes/UserRoutes.js';
+import PartenaireRoutes from "./routes/partenaireRoutes.js";
+import ParentRoutes from "./routes/parentRoutes.js";
+import dotenv from "dotenv";
+import user from "./routes/UserRoutes.js";
+import chatRoutes from "./routes/ChatRoutes.js";
+
 // Creating an express app
 const app = express();
+const server = http.createServer(app); // Create a server instance
 
 dotenv.config();
 // Setting the port number for the server (default to 9090 if not provided)
 const PORT = 9090 || process.env.PORT;
-const databaseName = 'PIM';
+const databaseName = "PIM";
 
 // Enabling debug mode for mongoose
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 
 // Setting the global Promise library
 mongoose.Promise = global.Promise;
 
 // Connecting to the MongoDB database
-mongoose.connect(`mongodb+srv://localhost:GWaB8yrPjyl265Vw@paymentforkids.vliqoot.mongodb.net/${databaseName}`)
-    .then(() => {
-        console.log(`Connected to  db`);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+mongoose
+  .connect(
+    `mongodb+srv://localhost:GWaB8yrPjyl265Vw@paymentforkids.vliqoot.mongodb.net/${databaseName}`
+  )
+  .then(() => {
+    console.log(`Connected to  db`);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // Enabling Cross-Origin Resource Sharing
 app.use(cors());
 
 // Using morgan for logging HTTP requests
-app.use(morgan('dev')); 
+app.use(morgan("dev"));
 
 // Parsing JSON request bodies
 app.use(express.json());
@@ -45,22 +53,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serving static files (images) from the 'public/images' directory
-app.use('/img', express.static('public/images'));
-
+app.use("/img", express.static("public/images"));
 
 // Importing the routes for the 'tests' resource
-app.use('/tests', Routes);
-app.use('/partenaire', PartenaireRoutes)
-app.use('/parent', ParentRoutes)
-
-app.use('/',user);
+app.use("/tests", Routes);
+app.use("/partenaire", PartenaireRoutes);
+app.use("/parent", ParentRoutes);
+app.use("/chat", chatRoutes), app.use("/", user);
 // Using custom middleware for handling 404 errors
 app.use(notFoundError);
 
 // Using custom middleware for handling general errors
-app.use(errorHandler); 
+app.use(errorHandler);
+
+// Start the socket server
+export const io = new Server(server);
+
+// Socket.io event handling
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
 // Starting the server and listening on the specified port
-app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
