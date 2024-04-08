d9722ef4f36bf6965fd1a55b28d0e4c8ce9fbaf9
yahya73 <80331784+yahya73@users.noreply.github.com>
2024-04-08 02:37:54 +0000

    Update dev_safe-payment-for-kids.yml

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,5 +1,5 @@
 import { validationResult } from 'express-validator';
-import Product from "../models/product.js";
+import Product from "../models/Product.js";
 import {sendNotification} from "./notificationController.js"
 
 
--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -1,4 +1,4 @@
-import Product from "../models/product.js";
+import Product from "../models/Product.js";
 import WishList from "../models/wishlist.js";
 
 
